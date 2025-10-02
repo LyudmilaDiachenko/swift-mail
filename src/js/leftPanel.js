25f6d02219af40ea4b520e51225606728e7fc74b
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-10-02 13:40:11 +0300

    changing category closes current email

--- src/js/leftPanel.js
+++ src/js/leftPanel.js
@@ -1,7 +1,7 @@
 import React from "react";
 import WriteLetter from "./newLetter";
 
-function LeftPanel({mails, setCurrentDraftId, setCurrentStatus}) {
+function LeftPanel({mails, setCurrentDraftId, setCurrentEmailId, setCurrentStatus}) {
   return (
     <div className="left-panel">
       <button className="write-letter">
@@ -12,10 +12,10 @@ function LeftPanel({mails, setCurrentDraftId, setCurrentStatus}) {
           >Написати листа
       </button>
       <ul className="list">
-          <li onClick={() => setCurrentStatus('inbox')}>Вхідні ({mails.filter(e=>e?.status==='inbox').length})</li>
-          <li onClick={() => setCurrentStatus('sent')}>Надіслані ({mails.filter(e=>e?.status==='sent').length})</li>
-          <li onClick={() => setCurrentStatus('spam')}>Спам ({mails.filter(e=>e?.status==='spam').length})</li>
-          <li onClick={() => setCurrentStatus('draft')}>Чернетки ({mails.filter(e=>e?.status==='draft').length})</li>
+          <li onClick={() => setCurrentStatus('inbox') || setCurrentEmailId(null)}>Вхідні ({mails.filter(e=>e?.status==='inbox').length})</li>
+          <li onClick={() => setCurrentStatus('sent') || setCurrentEmailId(null)}>Надіслані ({mails.filter(e=>e?.status==='sent').length})</li>
+          <li onClick={() => setCurrentStatus('spam') || setCurrentEmailId(null)}>Спам ({mails.filter(e=>e?.status==='spam').length})</li>
+          <li onClick={() => setCurrentStatus('draft') || setCurrentEmailId(null)}>Чернетки ({mails.filter(e=>e?.status==='draft').length})</li>
       </ul>
     </div>
   )
