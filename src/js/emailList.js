c2ef758ecef261e13796a2b19c79ae8ec91b11a6
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-10-06 20:55:17 +0300

    filter improvements

--- src/js/emailList.js
+++ src/js/emailList.js
@@ -1,20 +1,24 @@
 import React from "react";
+import EmptyMails from "./emptyMails";
 
 function EmailList({mails, setCurrentEmailId, currentStatus, searchRequest}) {
     const filteredMails = mails
         .filter(e => e?.status === currentStatus)
-        .filter(e => e?.title?.toLowerCase()?.includes(searchRequest?.toLowerCase()))
+        .filter(e => {
+            return e?.title?.toLowerCase()?.includes(searchRequest?.toLowerCase()) ||
+                   e?.sender?.toLowerCase()?.includes(searchRequest?.toLowerCase()) ||
+                   e?.description?.toLowerCase()?.includes(searchRequest?.toLowerCase())
+        })
     return (
-        <div className="right-panel">
+        <div>
             <ul className="mail-list">
                 {filteredMails.length ? filteredMails.sort((a, b) => a.date < b.date? 1 : -1).map((e, i)=>
-                    <li className="mail-list-item" key={e.id} onClick={_=>setCurrentEmailId(e.id)}>
+                    <li className="mail-list-item" key={e.id} onClick={_=>setCurrentEmailId(e.id)} title={e?.description}>
                         <span className="mail-sender">{e.sender || e.email}</span>
                         <span className="mail-header">{e.title}</span>
                         <span className="mail-date">{new Date(e.date).toLocaleDateString()}</span>
                     </li>
-                ) : <></>
-                // <EmptyMails {...{searchRequest, currentStatus}}/>
+                ) : <EmptyMails {...{searchRequest, currentStatus}}/>
                 }
             </ul>
         </div>
