693dd67f04be10418eda9ff1c042439bfd3da492
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-10-06 14:44:54 +0300

    small improvements

--- src/js/viewEmail.js
+++ src/js/viewEmail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Search from "./search";
+import { PiKeyReturnBold } from "react-icons/pi";
 
 function EmailList({mails, currentEmailId, setCurrentEmailId}) {
     const email = mails.filter(e => e?.id === currentEmailId)[0]
@@ -8,15 +9,17 @@ function EmailList({mails, currentEmailId, setCurrentEmailId}) {
         <div className="view-letter">
             <Search />
             <div className="view-box">
-                <div className="view-header">
-                    <h2>{email['title']}</h2>
-                    <p>{email['sender']} <span>{email['email']}</span></p>
+                <div className="header-container">
+                    <div className="view-header">
+                        <h2>{email['title']}</h2>
+                        <p>{email['sender']} <span> <a href={`mailto:${email['email']}`}>{email['email']}</a></span></p>
+                    </div>
+                    <div onClick={_=>setCurrentEmailId(null)}>
+                        <PiKeyReturnBold className="return-icon"/>
+                    </div>
                 </div>
-                <div>
+                <div className="view-description">
                     {email['description']}
-                </div>
-                <div>
-                    <button onClick={_=>setCurrentEmailId(null)}>Взад</button>
                 </div>
             </div>
         </div>
